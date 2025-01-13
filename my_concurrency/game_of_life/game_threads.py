85923ce2a1f960acf383d2a133b9ab2b0ce2dbf6
Harry Yang <hhy@google.com>
2025-01-13 11:02:30 -0800

    implement game of life with thread and asyncio

---
+++ my_concurrency/game_of_life/game_threads.py
@@ -0,0 +1,154 @@
+'''
+Add Thread for each step
+'''
+
+import time
+import logging
+import threading
+
+
+logging.basicConfig()
+LOG = logging.getLogger()
+LOG.setLevel(logging.DEBUG)
+
+TS = time.time()
+
+
+ALIVE = "*"
+EMPTY = "-"
+
+
+
+class Grid:
+    def __init__(self, height, width):
+        self.height = height
+        self.width = width
+        self.rows = []
+        for _ in range(self.height):
+            self.rows.append([EMPTY] * self.width)
+
+    def get(self, y, x):
+        return self.rows[y % self.height][x % self.width]
+
+    def set(self, y, x, state):
+        self.rows[y % self.height][x % self.width] = state
+
+    def __str__(self):
+        output = ""
+        for row in self.rows:
+            for cell in row:
+                output += cell
+            output += "\n"
+        return output
+
+
+class ColumnPrinter:
+    def __init__(self):
+        self.columns = []
+
+    def append(self, data):
+        self.columns.append(data)
+
+    def __str__(self):
+        row_count = 1
+        for data in self.columns:
+            row_count = max(row_count, len(data.splitlines()) + 1)
+
+        rows = [""] * row_count
+        for j in range(row_count): # j:= 1, 2, ..., rows
+            for i, data in enumerate(self.columns):
+                line = data.splitlines()[max(0, j - 1)]
+                if j == 0:
+                    padding = " " * (len(line) // 2)
+                    rows[j] += padding + str(i) + padding
+                else:
+                    rows[j] += line
+
+                if (i + 1) < len(self.columns):
+                    rows[j] += " | "
+
+        return "\n".join(rows)
+
+
+def count_neighbors(y, x, get_cell):
+    n_ = get_cell(y - 1, x + 0)  # North
+    ne = get_cell(y - 1, x + 1)  # Northeast
+    e_ = get_cell(y + 0, x + 1)  # East
+    se = get_cell(y + 1, x + 1)  # Southeast
+    s_ = get_cell(y + 1, x + 0)  # South
+    sw = get_cell(y + 1, x - 1)  # Southwest
+    w_ = get_cell(y + 0, x - 1)  # West
+    nw = get_cell(y - 1, x - 1)  # Northwest
+    neighbor_states = [n_, ne, e_, se, s_, sw, w_, nw]
+    count = 0
+    for state in neighbor_states:
+        if state == ALIVE:
+            count += 1
+    return count
+
+
+
+def game_logic(state, neighbors):
+    time.sleep(.1)
+    if state == ALIVE:
+        if neighbors < 2:
+            return EMPTY     # Die: Too few
+        elif neighbors > 3:
+            return EMPTY     # Die: Too many
+    else:
+        if neighbors == 3:
+            return ALIVE     # Regenerate
+    return state
+
+
+def step_cell(y, x, get_cell, set_cell):
+    state = get_cell(y, x)
+    neighbors = count_neighbors(y, x, get_cell)
+    next_state = game_logic(state, neighbors)
+    set_cell(y, x, next_state)
+
+
+
+def simulate(grid):
+    next_grid = Grid(grid.height, grid.width)
+    for y in range(grid.height):
+        for x in range(grid.width):
+            step_cell(y, x, grid.get, next_grid.set)
+    return next_grid
+
+def simulate_threaded(grid):
+    threads = []
+    next_grid = Grid(grid.height, grid.width)
+    for y in range(grid.height):
+        for x in range(grid.width):
+            thread = threading.Thread(target = step_cell, args=(y, x, grid.get, next_grid.set))
+            thread.start()
+            threads.append(thread)
+
+    for thread in threads:
+        thread.join()
+    return next_grid
+
+
+
+
+def main():
+    grid = Grid(5, 9)
+    grid.set(0, 3, ALIVE)
+    grid.set(1, 4, ALIVE)
+    grid.set(2, 2, ALIVE)
+    grid.set(2, 3, ALIVE)
+    grid.set(2, 4, ALIVE)
+    print(grid)
+    columns = ColumnPrinter()
+    for i in range(5):
+        columns.append(str(grid))
+        grid = simulate_threaded(grid)
+    print(columns)
+
+
+if __name__ == '__main__':
+    ts = time.time()
+    main()
+    te = time.time()
+    LOG.info(f"took {te - ts: .2f} seconds")
